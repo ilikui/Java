ff5c9d85aa7c9e09da57b8cda8533e1c32a0c1b2
Li <kui2.li@aptiv.com>
2022-03-07 14:52:34 +0800

    update by likui

---
+++ JavaFly/JavaFly/CH001/info/yueya/J001/HelloWorldApplet.java
@@ -0,0 +1,16 @@
+package info.yueya.J001;
+
+
+import java.awt.Graphics;
+
+@SuppressWarnings({ "removal", "serial" })
+public class HelloWorldApplet  extends java.applet.Applet{
+	
+	public void paint(Graphics g)
+	{
+		g.drawString("hello world", 5, 25);
+	}
+	
+	
+
+}
