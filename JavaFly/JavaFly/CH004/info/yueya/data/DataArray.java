1333de88f58040b9b1765ef8c830f6dc6c7cfda0
ilikui <2563043733@qq.com>
2022-06-06 09:33:13 +0800

    update by likui

---
+++ JavaFly/JavaFly/CH004/info/yueya/data/DataArray.java
@@ -0,0 +1,13 @@
+package info.yueya.data;
+
+/**
+ * 数组的使用
+ * @author rjc1gr
+ *
+ */
+
+public class DataArray {
+	
+	
+
+}
---
+++ JavaFly/JavaFly/CH004/info/yueya/data/IfCorrectTest.java
@@ -0,0 +1,18 @@
+package info.yueya.data;
+
+public class IfCorrectTest {
+
+	public static void main(String[] args) {
+		int age = 45;
+		if (age > 60) {
+
+			System.out.println("老年人");
+
+		} else if (age > 40) {
+			System.out.println("中年人");
+		} else if (age > 20) {
+			System.out.println("青年人");
+		}
+	}
+
+}
